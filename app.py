8d1675a3b8d9abf88814b14edc7579565e0621f2
Idhamm15 <idhamtamvanz123@gmail.com>
2023-11-02 13:16:21 +0700

    Update API Flask with SQLAlchemy

--- app.py
+++ app.py
@@ -16,40 +16,43 @@ class Mahasiswa(db.Model):
 # Buat tabel dalam database
 db.create_all()
 
-@app.route('/mahasiswa', methods=['GET', 'POST'])
-def mahasiswa():
-    if request.method == 'GET':
-        # Mengambil semua data mahasiswa
-        mahasiswas = Mahasiswa.query.all()
-        data = [{'id': m.id, 'nama': m.nama, 'umur': m.umur, 'alamat': m.alamat} for m in mahasiswas]
-        return jsonify(data)
+@app.route('/mahasiswa', methods=['GET'])
+def get_mahasiswa():
+    mahasiswas = Mahasiswa.query.all()
+    data = [{'id': m.id, 'nama': m.nama, 'umur': m.umur, 'alamat': m.alamat} for m in mahasiswas]
+    return jsonify(data)
 
-    if request.method == 'POST':
-        # Menambahkan data mahasiswa baru
-        data = request.get_json()
-        mahasiswa_baru = Mahasiswa(nama=data['nama'], umur=data['umur'], alamat=data['alamat'])
-        db.session.add(mahasiswa_baru)
-        db.session.commit()
-        return jsonify({'message': 'Data mahasiswa telah ditambahkan.'})
+@app.route('/mahasiswa', methods=['POST'])
+def create_mahasiswa():
+    data = request.get_json()
+    mahasiswa_baru = Mahasiswa(nama=data['nama'], umur=data['umur'], alamat=data['alamat'])
+    db.session.add(mahasiswa_baru)
+    db.session.commit()
+    return jsonify({'message': 'Data mahasiswa telah ditambahkan.'})
 
-
-@app.route('/mahasiswa/<int:id>', methods=['PUT', 'DELETE'])
-def update_delete_mahasiswa(id):
+@app.route('/mahasiswa/<int:id>', methods=['PUT'])
+def update_mahasiswa(id):
     mahasiswa = Mahasiswa.query.get(id)
-
     if not mahasiswa:
         return jsonify({'message': 'Mahasiswa tidak ditemukan'}), 404
 
-        mahasiswa.nama = data.get('nama', mahasiswa.nama)
-        mahasiswa.umur = data.get('umur', mahasiswa.umur)
-        mahasiswa.alamat = data.get('alamat', mahasiswa.alamat)
-        db.session.commit()
-        return jsonify({'message': 'Data mahasiswa telah diperbarui.'})
+    data = request.json
+    mahasiswa.nama = data.get('nama', mahasiswa.nama)
+    mahasiswa.umur = data.get('umur', mahasiswa.umur)
+    mahasiswa.alamat = data.get('alamat', mahasiswa.alamat)
+    db.session.commit()
+    return jsonify({'message': 'Data mahasiswa telah diperbarui.'})
 
-    if request.method == 'DELETE':
-        db.session.delete(mahasiswa)
-        db.session.commit()
-        return jsonify({'message': 'Data mahasiswa telah dihapus.'})
+@app.route('/mahasiswa/<int:id>', methods=['DELETE'])
+def delete_mahasiswa(id):
+    mahasiswa = Mahasiswa.query.get(id)
+    if not mahasiswa:
+        return jsonify({'message': 'Mahasiswa tidak ditemukan'}), 404
+
+    db.session.delete(mahasiswa)
+    db.session.commit()
+    return jsonify({'message': 'Data mahasiswa telah dihapus.'})
+
 
 
 if __name__ == '__main__':
